74885c772a049c3ac8e782736c1cc002b6975676
Walter Kaunda <walterkaunda@gmail.com>
2018-10-07 20:53:49 +0200

    About halfway into tutorial...
    Specifically @ 51:19.

---
+++ README.md
@@ -0,0 +1,3 @@
+# startingclojure
+
+Url shortener web app from [starting clojure](https://youtu.be/VVd4ow-ZcX0) tutorial.
---
+++ src/startingclojure/app.clj
@@ -0,0 +1,28 @@
+(ns startingclojure.app
+  (:use (compojure handler
+                   (core :only (GET POST defroutes))))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]))
+
+(defonce counter (atom 10000))
+
+(defonce urls (atom {}))
+
+(defn shorten
+  [url]
+  (let [id (swap! counter inc)
+        id (Long/toString id 36)]
+    (swap! urls assoc id url)
+    id))
+
+(defn homepage
+  [request]
+  (str @urls))
+
+(defn redirect
+  [id]
+  (response/redirect (@urls id)))
+
+(defroutes app
+  (GET "/" request (homepage request))
+  (GET "/:id" [id] (redirect id)))
