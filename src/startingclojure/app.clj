eaa68f4dc9304e0f8884d63f4f0a4d719b3ce02d
Walter Kaunda <walterkaunda@gmail.com>
2018-10-12 22:06:43 +0200

    Completed tutorial

--- src/startingclojure/app.clj
+++ src/startingclojure/app.clj
@@ -1,8 +1,10 @@
 (ns startingclojure.app
   (:use (compojure handler
                    (core :only (GET POST defroutes))))
-  (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :as response]))
+  (:require compojure.route
+            [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]
+            [net.cgrand.enlive-html :as enlive]))
 
 (defonce counter (atom 10000))
 
@@ -15,16 +17,27 @@
     (swap! urls assoc id url)
     id))
 
-(defn homepage
+(enlive/deftemplate homepage
+  (enlive/xml-resource "homepage.html")
   [request]
-  (str @urls))
+  [:#listing :li] (enlive/clone-for [[id url] @urls]
+                    [:a] (comp
+                           (enlive/content (format "%s : %s" id url))
+                           (enlive/set-attr :href (str \/ id)))))
 
 (defn redirect
   [id]
   (response/redirect (@urls id)))
 
-(defroutes app
+(defroutes app*
   (GET "/" request (homepage request))
-  (GET "/:id" [id] (redirect id)))
+  (GET "/:id" [id] (redirect id))
+  (POST "/shorten" request
+    (let [id (shorten (-> request :params :url))]
+      (response/redirect "/")))
+  (compojure.route/resources "/"))
+
+
+(def app (compojure.handler/site app*))
 
 (def server (jetty/run-jetty #'app {:port 8000 :join? false}))
